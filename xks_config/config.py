b255f826cd4b7c762b263cfe6a0d766f122003af
yoshiki.obata <yoshiki.obata@gmail.com>
2019-04-15 20:30:41 +0900

    add configs for xkeysnail

---
+++ xks_config/config.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+import re
+from xkeysnail.transform import *
+
+define_multipurpose_modmap({
+    Key.LEFT_ALT: [Key.MUHENKAN, Key.LEFT_ALT],
+    Key.RIGHT_ALT: [Key.HENKAN, Key.RIGHT_ALT]
+})
